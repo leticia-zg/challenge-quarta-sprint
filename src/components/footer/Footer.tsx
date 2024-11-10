ef73f1a9db9f8390940f13519392768823dce1ba
= <=>
2024-11-10 01:18:10 -0300

    Pequenos ajustes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,54 +1,73 @@
 import './Footer.css';
 import Link from "next/link";
+import Image from 'next/image'; // Importando o componente Image do Next.js
 
 const Footer = () => {
     return (
         <footer role="contentinfo">
             <div className="footer-content">
-                <img 
+                <Image 
                     src="/imgs/logobranca.png" 
                     alt="Logo Porto" 
                     className="logo-branca" 
-                    role="img" />
+                    role="img"
+                    width={200} // Definindo as dimensões, você pode ajustar conforme necessário
+                    height={100} // Definindo as dimensões, você pode ajustar conforme necessário
+                />
                 <section className="contatos" role="group">
                     <h3>Acompanhe a Porto nas Redes Sociais</h3>
                     <ul role="navigation">
                         <li>
                             <Link href="https://www.facebook.com/porto" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-                                <img 
+                                <Image 
                                     src="/imgs/facebook.png" 
-                                    alt="Logo do Facebook" />
+                                    alt="Logo do Facebook" 
+                                    width={40} // Ajuste as dimensões conforme necessário
+                                    height={40} // Ajuste as dimensões conforme necessário
+                                />
                             </Link>
                         </li>
                         <li>
                             <Link href="https://www.instagram.com/porto/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-                                <img 
+                                <Image 
                                     src="/imgs/instagram.png" 
-                                    alt="Logo do Instagram" />
+                                    alt="Logo do Instagram" 
+                                    width={40} // Ajuste as dimensões conforme necessário
+                                    height={40} // Ajuste as dimensões conforme necessário
+                                />
                             </Link>
                         </li>
                         <li>
                             <Link href="https://www.linkedin.com/company/porto/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-                                <img 
+                                <Image 
                                     src="/imgs/linkedin.png" 
                                     alt="Logo do LinkedIn" 
-                                    className="linkedin" />
+                                    className="linkedin"
+                                    width={40} // Ajuste as dimensões conforme necessário
+                                    height={40} // Ajuste as dimensões conforme necessário
+                                />
                             </Link>
                         </li>
                         <li>
                             <Link href="https://www.youtube.com/portoseguro" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
-                                <img 
+                                <Image 
                                     src="/imgs/youtube.png" 
                                     alt="Logo do YouTube" 
-                                    className="youtube" />
+                                    className="youtube"
+                                    width={40} // Ajuste as dimensões conforme necessário
+                                    height={40} // Ajuste as dimensões conforme necessário
+                                />
                             </Link>
                         </li>
                         <li>
                             <Link href="https://twitter.com/portoseguro" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-                                <img 
+                                <Image 
                                     src="/imgs/twitter.png" 
                                     alt="Logo do Twitter" 
-                                    className="twitter" />
+                                    className="twitter"
+                                    width={40} // Ajuste as dimensões conforme necessário
+                                    height={40} // Ajuste as dimensões conforme necessário
+                                />
                             </Link>
                         </li>
                     </ul>
@@ -56,17 +75,23 @@ const Footer = () => {
                 <section role="group">
                     <h3 className="h3-app">Baixe o App da Porto</h3>
                     <Link href="https://apps.apple.com/us/app/porto-seguros-cart%C3%A3o-e/id1511026277" target="_blank" rel="noopener noreferrer" aria-label="Baixar na App Store">
-                        <img 
+                        <Image 
                             src="/imgs/app store.png" 
                             alt="App Store" 
-                            className="appstore" />
+                            className="appstore"
+                            width={200} // Ajuste as dimensões conforme necessário
+                            height={60} // Ajuste as dimensões conforme necessário
+                        />
                     </Link>
                     <Link href="https://play.google.com/store/apps/details?id=br.com.portoseguro.experienciacliente.mundoporto&referrer=utm_source%3Dhub-vendas%26utm_medium%3Dcta-download%26utm_campaign%3Dhub-vendas-rodape&pli=1"
                         target="_blank" rel="noopener noreferrer" aria-label="Baixar no Google Play">
-                        <img 
+                        <Image 
                             src="/imgs/google play.png" 
                             alt="Google Play" 
-                            className="googleplay" />
+                            className="googleplay"
+                            width={200} // Ajuste as dimensões conforme necessário
+                            height={60} // Ajuste as dimensões conforme necessário
+                        />
                     </Link>
                 </section>
                 <div className="div-footer" role="contentinfo">
